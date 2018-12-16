7bffb4545d8982ef028da4a041613b92fd8385af
Manish Kumar <manish@urbanladder.com>
2018-12-16 10:08:54 +0530

    Added app repository module in component and inject module in main activity

--- app/src/main/java/com/example/github/di/ActivityBindingModule.kt
+++ app/src/main/java/com/example/github/di/ActivityBindingModule.kt
@@ -1,7 +1,14 @@
 package com.example.github.di
 
+import com.example.github.ui.main.MainActivity
+import com.example.github.ui.pullrequests.PullRequestFragmentModule
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Suppress("unused")
 @Module
-abstract class ActivityBindingModule
+abstract class ActivityBindingModule {
+    @ActivityScoped
+    @ContributesAndroidInjector(modules = [PullRequestFragmentModule::class])
+    internal abstract fun contributeMainActivity(): MainActivity
+}
--- app/src/main/java/com/example/github/di/AppComponent.kt
+++ app/src/main/java/com/example/github/di/AppComponent.kt
@@ -2,6 +2,7 @@ package com.example.github.di
 
 import com.example.github.MainApplication
 import com.example.github.di.viewmodel.ViewModelModule
+import com.example.github.repository.AppRepositoryModule
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
@@ -13,6 +14,7 @@ import javax.inject.Singleton
     AndroidSupportInjectionModule::class,
     AppModule::class,
     ViewModelModule::class,
+    AppRepositoryModule::class,
     ActivityBindingModule::class])
 interface AppComponent : AndroidInjector<MainApplication> {
     @Component.Builder
